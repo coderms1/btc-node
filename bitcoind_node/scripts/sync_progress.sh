27288968df394df06da6742ec15f337c80af0216
MS1 <157831131+coderms1@users.noreply.github.com>
2025-11-01 14:23:58 -0400

    Added some commentary

--- bitcoind_node/scripts/sync_progress.sh
+++ bitcoind_node/scripts/sync_progress.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+# Use this if you want an update every 10 minutes. 
+# If you've build the node_progress.sh script this may be redundant...up to you!
 watch -n 600 "bitcoin-cli getblockcount && bitcoin-cli getblockchaininfo | grep verificationprogress"
